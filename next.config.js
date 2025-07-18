226ababdc626c4f21466e43670dc0fa43515a533
webDevMatt-dot <mgluis630@gmail.com>
2025-07-18 17:15:15 +0200

    Fix: static export setup for Netlify

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', // This tells Next.js to do static export
+};
+
+module.exports = nextConfig;
