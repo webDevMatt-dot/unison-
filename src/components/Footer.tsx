532e9a76ed6bdf1e30f412462d271033dca9ca65
webDevMatt-dot <mgluis630@gmail.com>
2025-07-21 11:55:56 +0200

    UPDATE: footer stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,31 @@
 
-function Footer() {
+import "./Footer.css";
+
+const Footer = () => {
   return (
-    <footer className="footer" id="contact">
+    <footer className="footer">
+      <div className="footer-content">
+        {/*<h3 className="footer-logo">Unison</h3>*/}
+
+        <div className="footer-section">
+          <h4>Social Media</h4>
+          <p>Twitter: <strong>@UniData2019</strong></p>
+          <p>Instagram: <strong>@UniData2019</strong></p>
+          <p>WhatsApp: <a href="mailto:unidatabusinesssolutions@gmail.com">+27 60 489 7594</a></p>
+        </div>
+
+        <div className="footer-section">
+          <h4>Contact Details</h4>
+          <p>📞 Phone: <a href="tel:+27604897594">+27 60 489 7594</a></p>
+          <p>☎️ Landline: <a href="tel:+27816515179">+27 81 651 5179</a></p>
+          <p>📧 Email: <a href="mailto:admin@unidata.co.za">admin@unidata.co.za</a></p>
+        </div>
+      </div>
       <p>© {new Date().getFullYear()} Unison Consulting • All rights reserved</p>
+
     </footer>
   );
-}
+};
 
 export default Footer;
+
